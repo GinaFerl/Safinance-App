a10d7089d45e0e871f542c880098b616d622fc9e
GinaFerl <ginaferlindawati@gmail.com>
2025-08-07 14:25:57 +0700

    initial commit

---
+++ app/src/main/java/com/ginaf/safinance/ui/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.ginaf.safinance.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.ginaf.safinance.MainActivity
+import com.ginaf.safinance.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+          val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
